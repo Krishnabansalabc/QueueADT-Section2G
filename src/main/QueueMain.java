0522a8578151a9991de6b28f1b4824c34a9daec0
Krishnabansalabc <krishna.vbn123@gmail.com>
2019-01-17 10:01:27 +0530

    add enqueue updated

--- src/main/QueueMain.java
+++ src/main/QueueMain.java
@@ -5,12 +5,7 @@ public class QueueMain
 	public static void main(String[] args)
 	{
 		QueueADT queue1 = new QueueADT();
-		queue1.enqueue(11);
-		queue1.enqueue(10);
-		queue1.enqueue(9);
-		for(int i=0;i<queue1.queue.length;i++) {
-			System.out.print(queue1.queue[i]+" ");
-		}
+
 
 //		TODO add method call for enqueue
 
